44a7083f69ef117bc6e651a1c964e534b991374c
Aankur Bhatia <aank2007@gmail.com>
2016-10-17 10:30:50 -0400

    commit with some changes

--- yelp_cust_rev.R
+++ yelp_cust_rev.R
@@ -1,19 +1,42 @@
 library(stringr)
 library(jsonlite)
-library(rjson)
+
+library(readr)
+library(dplyr)
 list.files("./yelp_dataset_challenge_academic_dataset")
 list.files()
-untar("yelp_dataset_challenge_academic_dataset.tar",list=TRUE)
 
-install.packages("RJSONIO")
-library(RJSONIO)
 
-list.files()
+
 lines=readLines("./yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json")
 business=as.data.frame(t(sapply(lines,fromJSON)),row.names=FALSE)
 rm(lines)
-head(business)
-#now read the review file
 
-lines= readLines("./yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json",n = 200000)
-review=as.data.frame(t(sapply(lines, fromJSON)),row.names=FALSE)
+
+#now read the review file
+#
+lines= read_lines("./yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json",n_max =  200000,progress=FALSE)
+reviews_combined <- str_c("[", str_c(lines, collapse = ", "), "]")
+
+
+reviews=fromJSON(reviews_combined)%>%
+  flatten()%>%
+  tbl_df()
+
+
+
+rm(lines)
+library(dplyr)
+library(tidytext)
+?str_c
+review$votes=NULL
+review=review%>%
+  flatten()%>%
+  tbl_df()
+review_words=review%>%
+  unnest_tokens(word,text)
+
+rm(review_words)
+head(review_words,6)
+  # filter(!word %in% stop_words$word,
+  #        str_detect(word,"^[a-z']+$"))
